00e04b3893a3d737455d0e565b64fcc809ea5c10
Raj05RK <rajkumarayyadurai03@gmail.com>
2025-04-20 15:09:19 +0530

    Add files via upload

---
+++ TaskService.java
@@ -0,0 +1,28 @@
+package com.task.TaskManager;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaskService {
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public Task saveTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
+
+    public Task updateTask(Task task) {
+        return taskRepository.save(task);
+    }
+}
---
+++ User.java
@@ -0,0 +1,25 @@
+package com.task.TaskManager;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Task> tasks;
+}
